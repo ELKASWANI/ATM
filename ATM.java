b277bf7ccc4c5257c1cf0ef18d42eebedd33a2f0
ELKASWANI <57457392+ELKASWANI@users.noreply.github.com>
2020-05-10 22:16:32 -0400

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,12 +1,10 @@
 /*
-An ATM machine simulator. Create an Account class with all the attributes
-that characterize a bank account, and all the methods needed, including
-toString() and equals(). You have provide operations such as deposit,
-withdraw, transfer between accounts and print balance. Assume the ATM
-only manages 3 accounts. Challenge: the account could be configured to
+An ATM machine simulator. i provided operations such as deposit,
+withdraw and print balance. The Challenge: the account could be configured to
 always withdraw money with the least amount of bills possible. For
 instance, if 90 CAD is requested, the least amount would be 1 bill of 50 CAD
 and 2 bills of 20 CAD.
+
 Written By MOHAMMAD EL KASWANI
 
 i did two programs because i have a problem,
